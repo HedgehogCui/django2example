03649e494600b8e7473b98efd6c37e77b017729f
Hedgehog <xitong_cui@163.com>
2019-09-18 17:00:14 +0800

    Chapter 2

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'taggit',
     'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -119,3 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.exmail.qq.com'
+EMAIL_HOST_USER = 'cuixitong@boyunvision.com'
+EMAIL_HOST_PASSWORD = 'CXt1989@boyun'
+EMAIL_PORT = '465'
+EMAIL_USE_SSL = True
